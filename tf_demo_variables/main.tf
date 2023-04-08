ccbf3f8cfd72ac874ae18357c998783e9de9f77b
Noil Oomman <noil.oomman@yahoo.com>
2023-04-09 08:06:57 +1000

    rds database name

--- tf_demo_variables/main.tf
+++ tf_demo_variables/main.tf
@@ -215,7 +215,7 @@ resource "aws_db_instance" "default" {
   engine_version         = var.rds_instance.engine_version
   instance_class         = var.rds_instance.instance_class
   multi_az               = var.rds_instance.multi_az
-  name                   = var.rds_instance.name
+  db_name                = var.rds_instance.name
   username               = var.user_information.username
   password               = var.user_information.password
   skip_final_snapshot    = var.rds_instance.skip_final_snapshot
